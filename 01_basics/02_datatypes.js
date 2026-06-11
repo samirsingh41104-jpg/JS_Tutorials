938cfa4f1be184338b745493a20afe24e803b86a
samirsingh41104-jpg <samirsingh41104@gmail.com>
2026-06-11 10:25:58 +0000

    data_types

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,40 @@
+"use strict"; // it treats all JS code as newer version of JS (even the older ones)
+
+//alert("hello") // we are using nodejs, not browser
+
+/*console.log(3+2) console.log("Hi!")
+This is wrong 
+console.log(3+2); console.log("Hi!")
+This is right but less readable
+
+similarly 
+
+console.log(3
+    +2)
+console("Hi!")
+also correct but not easily readable    
+*/
+
+//make code readable
+console.log(3+2);
+console.log("Hi!");
+
+let name = "Sam"
+let age = 18
+let isLoggedIn = false
+let state; //undefined
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+console.log(typeof age);
+
+console.log(typeof undefined); // type of undefined => undefined 
+console.log(typeof null); // type of null => object
