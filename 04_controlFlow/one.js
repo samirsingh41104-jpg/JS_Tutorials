8f16093b964d387b2a74897243d2dee7d2f908d5
samirsingh41104-jpg <samirsingh41104@gmail.com>
2026-06-23 16:50:40 +0000

    till if else injs

---
+++ 04_controlFlow/one.js
@@ -0,0 +1,21 @@
+//Control Flow or Logic Flow is the order in which the computer executes statements in a script. By default, the computer executes statements from top to bottom. However, we can change this order using control flow statements.
+
+// if statement 
+
+const isUserLoggedIn = true;
+
+if (isUserLoggedIn) {
+    console.log('Welcome back, user!');
+} 
+
+if(2 !== "2"){
+    console.log("executed!");
+}
+
+const temperature = 30;
+
+if (temperature > 25) {
+    console.log("It's a hot day!");
+} else {
+    console.log("It's a cool day!");
+}
