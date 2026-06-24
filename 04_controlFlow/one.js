f1003f78219b92aed75d25200097d479e958f779
samirsingh41104-jpg <samirsingh41104@gmail.com>
2026-06-24 08:01:12 +0000

    if elif in js

--- 04_controlFlow/one.js
+++ 04_controlFlow/one.js
@@ -19,3 +19,37 @@ if (temperature > 25) {
 } else {
     console.log("It's a cool day!");
 }
+
+const score = 200
+if(score > 100){
+    const power = "Fly"
+    console.log(`User Power: ${power}`);
+} 
+const balance = 2000
+//if(balance > 500) console.log("test"), console.log("test2");
+
+if(balance < 500){
+    log("Balance is low");
+} else if (balance < 750){
+    console.log("Balance is moderate");
+} else if (balance < 900){
+    console.log("Balance is good");
+} else {
+    console.log("Balance is excellent");
+}
+
+const userLoggedIn = true;
+const debitCard = true;
+const loggedInfromGoogle = false;
+const loggedInfromEmail = true;
+
+if(userLoggedIn && debitCard){
+    console.log("You can make a purchase");
+} else {
+    console.log("You cannot make a purchase");
+}
+if(loggedInfromGoogle || loggedInfromEmail){
+    console.log("You can access the account");
+} else {
+    console.log("You cannot access the account");
+}
